a4909c19fb1afd2c76608cdb25bff370d1610fde
benbenye <1203846281@qq.com>
2018-04-09 18:36:07 +0800

    q

--- src/config.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by bby on 18/4/8.
- */
-export default {
-  repoPath: "/repos/benbenye/git-blog"
-};
--- src/store/data.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by bby on 18/4/1.
- */
-export default {
-  token: ""
-};
