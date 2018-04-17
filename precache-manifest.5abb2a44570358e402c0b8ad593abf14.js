5da73cbe00f21001fa731bee9ac417f02ce3790b
benbenye <1203846281@qq.com>
2018-04-17 09:34:14 +0800

    init1

---
+++ precache-manifest.5abb2a44570358e402c0b8ad593abf14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6388f4b4d4f39d75a88",
+    "url": "/js/vendor.b6388f4b.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "4c1a3de98542e67527ee",
+    "url": "/js/app.4c1a3de9.js"
+  },
+  {
+    "revision": "dc854a1ed5ebdc2e03997a4b07320703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1a3de98542e67527ee",
+    "url": "/css/app.40d80605.css"
+  }
+];
